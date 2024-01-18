afa1a4c3c06ac8581905c16f941aecbf36bcf7a5
CartePokemon <157039637+CartePokemon@users.noreply.github.com>
2024-01-18 17:29:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-    <br/><br/><br/><br/><br/><br/>
+    
         <Header title="Cartes Pokémon : Retour sur un phénomène mondialement populaire" />
 <div>
 <div id="__next">
-<div class="container">    
+<div class="container">
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 <h1 class="title">Cartes Pok&eacute;mon : Retour sur un ph&eacute;nom&egrave;ne mondialement populaire</h1>
 <p>D&eacute;couvrez tout ce qu'il faut savoir sur les cartes Pok&eacute;mon dans cette page.&nbsp;</p>
 <h2>L'&Eacute;mergence des Cartes Pok&eacute;mon</h2>

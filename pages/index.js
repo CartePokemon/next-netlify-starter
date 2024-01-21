ff9ac007cb3c5125bbaee263c2890669fb78804e
CartePokemon <157039637+CartePokemon@users.noreply.github.com>
2024-01-21 11:31:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
     <div>
       <Head>
         <title>Cartes Pokémon : Retour sur un phénomène mondialement populaire</title>
